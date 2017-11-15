5ef5c3b539ad0ed7a1667d238963547b1aed4914
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-11-15 02:40:20 +0000

    word count&color change

--- public/scripts/composer-charcounter.js
+++ public/scripts/composer-charcounter.js
@@ -4,5 +4,10 @@ $(document).ready(function() {
     var length = $(this).val().length;
     var length = maxLength-length;
     $('.counter').text(length);
+    if(length < 0) {
+    $('.counter').css('color','red')
+  } else {
+    $('.counter').css('color','black')
+    };
   });
-});
+})
