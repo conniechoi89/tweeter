f249bc05272508f2a48f244f15f5dee89cf83f58
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-11-16 05:03:25 +0000

    make changes on fetching tweets with ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,24 +5,25 @@
  */
 
 
-    // var name = data.user.name;
+    // var name = data.user.name
     // var avatars = data.user.avatars.regular;
     // var handle = data.user.handle;
     // var content = data.content.text;
     // var created_at = data.created_at;
     // var $tweet = $('.article.tweet').addClass('tweet');
 
-function escape(str) {
-    return str;
-    var div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-}
+$(function() {
 
-$(document).ready(function() {
+  var $tweetsContainer = $('.tweet-container');
+
+  function escape(str) {
+      return str;
+      var div = document.createElement('div');
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+  }
 
   function createTweetElement(tweetData) {
-
     var article = `<article class='tweet'>
           <header>
             <img src="${escape(tweetData.user.avatars.regular)}" alt="avatars" />
@@ -46,83 +47,36 @@ $(document).ready(function() {
 
   function renderTweets(tweetData) {
     tweetData.forEach(function(tweet) {
-      var newTweet= createTweetElement(tweet)
-      $('.tweet-dash').prepend(newTweet);
+      $tweetsContainer.prepend(createTweetElement(tweet));
     });
   };
-  //renderTweets(tweetData);
-
-//   $('.tweet-dash').append(createTweetElement(tweetData));
-
- //   // loops through tweets
-//     // calls createTweetElement for each tweet
-//     // takes return value and appends it to the tweets container
 
 
-  function loadTweets(){
-    //makes an ajax call to load the data from /tweets
-    $.ajax({
-      type: "GET",
-      url: "/tweets",
-      //data: $form.serialize(),
-      success: function(result){
-        console.log("success", result);
-        renderTweets(result);
-      },
-      error: function(error){
-        console.log("error", error);
-      }
-    })
+  function loadTweets() {
+
+    $.get('/tweets')
+      .success(tweets => renderTweets(tweets))
+      .error(error => console.log('error', error));
+
   }
+
+  function submitTweet(e) {
+    e.preventDefault();
+    const $form = $(this);
+
+    $.post('/tweets', $form.serialize())
+      .success(() => loadTweets())
+      .error(data => alert(data.error));
+
+  }
+
   loadTweets();
 
-
-  function submitTweet(event) {
-    event.preventDefault();
-    const $form = $(this);
-    $.ajax({
-      type: "POST",
-      url: "/tweets",
-      data: $form.serialize(),
-      success: function(result){
-        console.log("success", result);
-        var newTweet = {
-              "user": {
-                "name": "Connie",
-                "avatars": {
-                  "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-                  "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-                  "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-                },
-                "handle": "@connie_great"
-              },
-              "content": {
-                "text": $('#tweetText').val()
-              },
-              "created_at": 1461113796368
-            }
-        var temp = createTweetElement(newTweet)
-        $('.tweet-dash').prepend(temp);
-      },
-      error: function(error){
-        console.log("error", error);
-      }
-    })
-      // .done(() => {
-      //   const type = $form.find('input[name="type"]').val();
-      //   console.log(type);
-      //   //$('.tweet-dash').prepend(`<textarea>${(form)}</textarea>`);
-      // })
-  }
-
-  // const $form = $('.new-tweet');
-
-  // $(".new-tweet form").on("submit", submitTweet);
-
-
-  // function loadTweets()
+  $("form").on("submit", submitTweet);
 
 });
+
+
   // //Two methods:
   // //1. the method of id
   // <input type="text" id="myTest" class="mygod" />
